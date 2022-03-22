6249ce5dbf243933e50e0f8a362367fc78bf86dd
Alvaro_Aquije <alvarodevcode@outlook.com>
2022-03-21 22:51:09 -0500

    chore: using dripsy api

--- views/Home.tsx
+++ views/Home.tsx
@@ -1,27 +1,30 @@
 import { NativeStackScreenProps } from '@react-navigation/native-stack'
-import { Text, View } from 'dripsy'
+import { styled, Text, View } from 'dripsy'
 import { FC } from 'react'
 import { TouchableOpacity } from 'react-native'
 import { RootStackParamList } from '../App'
 
 type Props = NativeStackScreenProps<RootStackParamList, 'Home'>
 
+const StyledTouchableOpacity = styled(TouchableOpacity)({
+  borderColor: '$background',
+  borderRadius: 10,
+  borderWidth: 1,
+  padding: 10,
+  margin: 10,
+})
+
 const Home: FC<Props> = ({ navigation }) => {
+  const handlePress = () => {
+    navigation.navigate('Config', { name: 'Jane' })
+  }
+
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
       <Text>Open up App.tsx to start working on your app!</Text>
-      <TouchableOpacity
-        style={{
-          borderColor: 'black',
-          borderRadius: 10,
-          borderWidth: 1,
-          padding: 10,
-          marginVertical: 10,
-        }}
-        onPress={() => navigation.navigate('Config', { name: 'Jane' })}
-      >
+      <StyledTouchableOpacity onPress={handlePress}>
         <Text>go to other screen</Text>
-      </TouchableOpacity>
+      </StyledTouchableOpacity>
     </View>
   )
 }
